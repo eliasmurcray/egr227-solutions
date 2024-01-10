b136d682e96609a32e989d3d291f6ed84e02d9a8
eliasmurcray <eliasmurcray@gmail.com>
2024-01-10 10:56:23 -0800

    1-3

---
+++ 1-3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::sort;
+
+int main() {
+	vector<double> scores;
+	
+	double n;
+	cin >> n;
+	while(n >= 0) {
+		scores.push_back(n);
+		cin >> n;
+	};
+
+	sort(scores.begin(), scores.end());
+
+	double range = scores[scores.size() - 1] - scores[0];
+
+	double mean = 0;
+	for (int i = 0; i < scores.size(); i++) {
+		mean += scores[i];
+	}
+	mean /= (double) scores.size();
+
+	double median = scores.size() % 2
+		? scores[scores.size() / 2]
+		: (scores[scores.size() / 2 - 1] + scores[scores.size() / 2]) / 2.;
+	
+	cout << "Mean: " << mean << "\nMedian: " << median << "\nRange: " << range << "\n";
+
+	return 0;
+}
